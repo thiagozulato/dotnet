8ac293a174ddab212a229c95ff8e30e53f59afd1
Thiago Zulato <thiago.zulato@newconsoftware.com.br>
2018-11-08 20:59:29 -0200

    added kafka project for testing

---
+++ kafka-dotnet/kafka-console/consumer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using Confluent.Kafka;
+
+namespace consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conf = new ConsumerConfig
+            { 
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                // Note: The AutoOffsetReset property determines the start offset in the event
+                // there are not yet any committed offsets for the consumer group for the
+                // topic/partitions of interest. By default, offsets are committed
+                // automatically, so in this example, consumption will only start from the
+                // eariest message in the topic 'my-topic' the first time you run the program.
+                AutoOffsetReset = AutoOffsetResetType.Earliest
+            };
+
+            using (var c = new Consumer<string, string>(conf))
+            {
+                
+                c.Subscribe("topic_test");
+
+                bool consuming = true;
+                // The client will automatically recover from non-fatal errors. You typically
+                // don't need to take any action unless an error is marked as fatal.
+                c.OnError += (_, e) => consuming = !e.IsFatal;
+
+                while (consuming)
+                {
+                    try
+                    {
+                        var cr = c.Consume();
+                        Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        Console.WriteLine($"Key {cr.Key} - Header assign for key {cr.Headers[0].Key} - {cr.Headers[0].Value.ToEncodingString()}");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+                
+                // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                c.Close();
+            }
+        }
+    }
+
+    static class ByteEnxtensions
+    {
+        public static string ToEncodingString(this byte[] value)
+        {
+            return Encoding.UTF8.GetString(value);
+        }
+    }
+}
---
+++ kafka-dotnet/kafka-console/producer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+namespace producer
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            // A Producer for sending messages with null keys and UTF-8 encoded values.
+            using (var p = new Producer<string, string>(config))
+            {
+                string message = string.Empty;
+                
+                do 
+                {
+                    Console.WriteLine("Digite uma mensagem para o kafka");
+                    message = Console.ReadLine();
+
+                    var messageKafka = new Message<string, string> { Value = message };
+                    messageKafka.Key = "cliente-assinatura-123456";
+                    messageKafka.Headers = new Headers();
+                    messageKafka.Headers.Add("clientId", "181781723".ToByte());
+
+                    try
+                    {
+                        var dr = await p.ProduceAsync("topic_test", messageKafka);
+                        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                    }
+                    catch (KafkaException e)
+                    {
+                        Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                    }
+                } while (message != "exit");
+            }
+        }
+    }
+
+    static class StringEnxtensions
+    {
+        public static byte[] ToByte(this string value)
+        {
+            return Encoding.UTF8.GetBytes(value);
+        }
+
+        public static string ToStringJson(this string value)
+        {
+            var dic = new Dictionary<string, string>();
+            var values = value.Split(";");
+
+            foreach (var valuesSequence in values)
+            {
+                var keys = valuesSequence.Split(":");
+
+                dic.Add(Convert.ToString(keys[0]), Convert.ToString(keys[1]));
+            }
+
+            return JsonConvert.SerializeObject(dic);
+        }
+    }
+}
